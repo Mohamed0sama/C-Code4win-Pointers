665e54087c19a1879eaefb7f3c08373ea6f74818
Mohamed0sama <86002865+Mohamed0sama@users.noreply.github.com>
2022-11-19 22:15:52 +0200

    Create print_unique.c

---
+++ Array/print_unique.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main(void) {
+	int arr[11]={1, 2, 3, 4, 5, 5, 6, 6, 6, 3, 2};
+	int freq[11]={0};
+	for (int i = 0; i < 11; i++) {
+		freq[arr[i]]++;
+	}
+	for (int i = 0; i < 11; i++) {
+		if (freq[i] == 1) {
+			printf("%d  ", i);
+		}
+	}
+}
